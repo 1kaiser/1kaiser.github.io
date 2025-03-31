a60208403c66eea59885e8dcc91900266d44729f
Kaiser Roy <schlboy26@gmail.com>
2025-03-31 18:49:05 +0530

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import { render } from 'solid-js/web';
+import { Router } from "@solidjs/router";
+import './styles.css';
+import App from './App';
+
+render(() => (
+  <Router>
+    <App />
+  </Router>
+), document.getElementById('root'));
