712ce171983dfe9af0d47dab6e8ce2f7fc99e5c3
Kaiser Roy <schlboy26@gmail.com>
2025-06-29 12:32:59 +0530

    Update models.js
    chicken biryani with eggs and potatoes

--- models/models.js
+++ models/models.js
@@ -57,6 +57,12 @@ window.modelsConfig = [
     title: "Kaiser Roy",
     description: "Kaiser_Roy",
     alt: "Kaiser Roy"
+  },
+  {
+    url: "https://github.com/1kaiser/1kaiser.github.io/releases/download/1/Chicken_Biryani.glb",
+    title: "chicken biryani",
+    description: "chicken biryani with eggs and potatoes",
+    alt: "chicken biryani with eggs and potatoes"
   }
 ];
 
